6ef280c84d3c873e01fd4cf0f383cae2849b6cc3
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-04-01 19:09:22 +0300

    Call getMethod/getDeclaredMethod depending on visibility

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/DescriptorBasedProperty.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/DescriptorBasedProperty.kt
@@ -17,6 +17,7 @@
 package kotlin.reflect.jvm.internal
 
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.serialization.ProtoBuf
 import org.jetbrains.kotlin.serialization.deserialization.NameResolver
@@ -39,7 +40,7 @@ abstract class DescriptorBasedProperty(computeDescriptor: () -> PropertyDescript
     protected val descriptor: PropertyDescriptor by ReflectProperties.lazySoft(computeDescriptor)
 
     // null if this is a property declared in a foreign (Java) class
-    private val protoData: PropertyProtoData? by ReflectProperties.lazyWeak @p {(): PropertyProtoData? ->
+    private val protoData: PropertyProtoData? by ReflectProperties.lazyWeak @p { ->
         val property = DescriptorUtils.unwrapFakeOverride(descriptor) as? DeserializedPropertyDescriptor
         if (property != null) {
             val proto = property.proto
@@ -60,13 +61,15 @@ abstract class DescriptorBasedProperty(computeDescriptor: () -> PropertyDescript
     open val getter: Method? by ReflectProperties.lazySoft {
         val proto = protoData
         if (proto == null || !proto.signature.hasGetter()) null
-        else container.findMethodBySignature(proto.signature.getGetter(), proto.nameResolver)
+        else container.findMethodBySignature(proto.signature.getGetter(), proto.nameResolver,
+                                             descriptor.getGetter()?.getVisibility()?.let { Visibilities.isPrivate(it) } ?: false)
     }
 
     open val setter: Method? by ReflectProperties.lazySoft {
         val proto = protoData
         if (proto == null || !proto.signature.hasSetter()) null
-        else container.findMethodBySignature(proto.signature.getSetter(), proto.nameResolver)
+        else container.findMethodBySignature(proto.signature.getSetter(), proto.nameResolver,
+                                             descriptor.getSetter()?.getVisibility()?.let { Visibilities.isPrivate(it) } ?: false)
     }
 
     override fun equals(other: Any?): Boolean =
